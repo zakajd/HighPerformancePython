cc8d3573a2cc32f27a9747eafe8a61267cbdcc5d
Jamil Zakirov <djamilzak@gmail.com>
2019-11-28 00:53:00 +0300

    Task7 no parallel

--- integral.py
+++ integral.py
@@ -12,15 +12,37 @@ except FileExistsError as err:
     pass
 
 
-# Function for which we compute integral
 def f(x):
-    return 1 / x
+    """ Function to integrate
+    Args:
+        x: value to count
+    """
+    return 1 / np.sqrt(1 + x ** 2)
 
-# Analytical integral
 def g(x):
-    return np.log(x)
+    """ Analytical expression of f(x)
+    Args:
+        x: func value
+    """
+    return np.log(x + np.sqrt(1 + x ** 2))
 
-def integrate(f, g=None, limits=(1, 2), nodes=100, rank=None, size=None, comm=None):
+# from functools import reduce
+
+def integrate(f, limits=(5, 7), num_steps=100):
+    """Compute function integral by using trapezoid rule
+    Args:
+        f: function to integrate
+        g: analytical integral, for self check and error estimation
+        limits: (a, b)
+        steps: # of points to split the limit
+    """
+    steps = np.linspace(limits[0], limits[1], num=num_steps, endpoint=True)
+    step_size = (limits[1] - limits[0]) / num_steps
+    func_values = [f(x) for x in steps]
+    result = (sum(func_values) * 2 - func_values[0] - func_values[1]) * 0.5 * step_size
+    return result
+
+def integrate_parallel(f, g=None, limits=(5, 7), nodes=100):
     """Compute function integral by using trapezoid rule
     Args:
         f: function to integrate
@@ -28,21 +50,33 @@ def integrate(f, g=None, limits=(1, 2), nodes=100, rank=None, size=None, comm=No
         limits: (a, b)
         nodes: # of points to split the limit
     """
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
     all_nodes = np.linspace(limits[0], limits[1], num=nodes)
     left, right = rank * nodes // size, (rank + 1) * nodes // size
     individual_nodes = list(all_nodes[left:right]) # convert to Python object
     if rank == size - 1:
         individual_nodes = all_nodes[left:] # leftmost process take the rest
-    # print(f'My rank: {rank}, world size: {size}, {individual_nodes}')
+    print(f'My rank: {rank}, world size: {size}, {individual_nodes}')
     comm.Barrier()
     partial_result = [(x, f(x)) for x in individual_nodes]
     
-    final_result = comm.gather(partial_result, root=0)
+    data = comm.gather(data, root=0)
+    
+    sendbuf = np.empty(2, dtype = 'i')
+    sendbuf[0] = 0
+    sendbuf[1] = rank
+    
+    # Recieve data
+    if rank == 0:
+        recvbuf = np.empty(2, dtype = 'i')
+    else:    
+        recvbuf = None
+        
+    if rank == 0:
+        printf(f"{nodes} trapezoids, [a, b] = [{limits}], result = {total_integral}")
 
-    if rank: # rank != 0
-        assert final_result is None
-
-    return final_result
 
 def main(args):
     comm = MPI.COMM_WORLD
