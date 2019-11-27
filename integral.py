c64100ea38175461643e6e11b416d5090d67a721
Jamil Zakirov <djamilzak@gmail.com>
2019-11-28 02:00:45 +0300

    Task 7 with parallel anti-speedup

--- integral.py
+++ integral.py
@@ -28,7 +28,7 @@ def g(x):
 
 # from functools import reduce
 
-def integrate(f, limits=(5, 7), num_steps=100):
+def integrate(f, limits=(5, 7), num_steps=100000):
     """Compute function integral by using trapezoid rule
     Args:
         f: function to integrate
@@ -42,57 +42,31 @@ def integrate(f, limits=(5, 7), num_steps=100):
     result = (sum(func_values) * 2 - func_values[0] - func_values[1]) * 0.5 * step_size
     return result
 
-def integrate_parallel(f, g=None, limits=(5, 7), nodes=100):
-    """Compute function integral by using trapezoid rule
-    Args:
-        f: function to integrate
-        g: analytical integral, for self check and error estimation
-        limits: (a, b)
-        nodes: # of points to split the limit
-    """
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-    size = comm.Get_size()
-    all_nodes = np.linspace(limits[0], limits[1], num=nodes)
-    left, right = rank * nodes // size, (rank + 1) * nodes // size
-    individual_nodes = list(all_nodes[left:right]) # convert to Python object
-    if rank == size - 1:
-        individual_nodes = all_nodes[left:] # leftmost process take the rest
-    print(f'My rank: {rank}, world size: {size}, {individual_nodes}')
-    comm.Barrier()
-    partial_result = [(x, f(x)) for x in individual_nodes]
-    
-    data = comm.gather(data, root=0)
-    
-    sendbuf = np.empty(2, dtype = 'i')
-    sendbuf[0] = 0
-    sendbuf[1] = rank
-    
-    # Recieve data
-    if rank == 0:
-        recvbuf = np.empty(2, dtype = 'i')
-    else:    
-        recvbuf = None
+
+def main(parallel=False):
+    if parallel:
+        comm = MPI.COMM_WORLD
+        rank = comm.Get_rank()
+        size = comm.Get_size()
         
-    if rank == 0:
-        printf(f"{nodes} trapezoids, [a, b] = [{limits}], result = {total_integral}")
+        all_nodes = np.linspace(5, 7, num=size + 1)
+        S_d = integrate(f, limits=all_nodes[rank:rank+2], num_steps=100000 // size)
+        comm.Barrier()
 
+        S_d = comm.gather(S_d, root=0)
 
-def main(args):
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-    size = comm.Get_size()
-    final_result = integrate(f, g=None, limits=(1, 10), nodes=10, rank=rank, size=size, comm=comm)
-    if rank == 0:
-        x = [t[0] for t in final_result]
-        y = [t[1] for t in final_result]
-        plt.plot(y, x)
-        plt.savefig('imgs/integral.png')
-        # print(final_result)
+        if rank == 0:
+#             print(sum(S_d))
+            return sum(S_d)
+    else:
+        S_d = integrate(f, limits=(5, 7), num_steps=100000)
+#         print(S_d)
+        return S_d
+        
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("Task 7")
-    parser.add_argument("-p",  help="# of proccesses")
+    parser.add_argument("-parallel",  help="0 - not parallel, 1 - parallel", type=bool)
     args = parser.parse_args()
-    main(args)
+    main(args.parallel)
 
